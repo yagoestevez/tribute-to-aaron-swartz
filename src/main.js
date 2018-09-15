4ba5367547f03d8a1d0943c78cb0a4944fee4b5a
Yago Estevez <inbox@yagoestevez.com>
2018-09-15 10:00:59 +0200

    Build basic navigation and section placeholders. Add bootstrap source. Implement scrollspy and smooth scrolling. Add styling.

--- src/main.js
+++ src/main.js
@@ -3,5 +3,73 @@
  */
 require( "babel-runtime/regenerator" );
 require( './index.html'              );
-require( './main.scss'                );
+require( './main.scss'               );
 
+const makeNavLinksSmooth = ( ) => {
+  const navLinks = document.querySelectorAll( '.nav-link' );
+
+  for ( let n in navLinks ) {
+    if ( navLinks.hasOwnProperty( n ) ) {
+      navLinks[ n ].addEventListener( 'click', e => {
+        e.preventDefault( );
+        document.querySelector( navLinks[ n ].hash )
+          .scrollIntoView( {
+            behavior: "smooth"
+          } );
+      } );
+    }
+  }
+}
+
+const spyScrolling = ( ) => {
+  const sections = document.querySelectorAll( '.section' );
+  const navBar   = document.querySelector( '#navbar' );
+
+  window.onscroll = ( ) => {
+    let scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if ( sections[ 0 ].offsetTop === scrollPos ) {
+      navBar.classList.remove( 'bg-black' );
+      navBar.classList.add( 'bg-transparent' );
+    } else {
+      console.log( sections[ 0 ].offsetTop, scrollPos )
+      navBar.classList.remove( 'bg-transparent' );
+      navBar.classList.add( 'bg-black' );
+    }
+
+    scrollPos += window.innerHeight / 2;
+
+    for ( let s in sections )
+      if ( sections.hasOwnProperty( s ) && sections[ s ].offsetTop <= scrollPos ) {
+        const id = sections[ s ].id;
+        document.querySelector( '.active' ).classList.remove( 'active' );
+        document.querySelector( `a[href*=${ id }]` ).parentNode.classList.add( 'active' );
+      }
+  }  
+}
+
+makeNavLinksSmooth( );
+spyScrolling( );
+
+
+/*
+const sections = document.querySelectorAll( '.section' );
+
+window.onscroll = ( ) => {
+  const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+
+  for ( let s in sections ) {
+    if ( sections.hasOwnProperty( s ) ) {
+      if ( sections[ s ].offsetTop <= scrollPos ) {
+        const id   = sections[ s ].id;
+        const curr = document.querySelector( '.active' );
+        const next = document.querySelector( `a[href*=${ id }]` );
+        if ( curr.id !== id ) {
+          curr.classList.remove( 'active' );
+          next.parentNode.classList.add( 'active' );
+        }
+      }
+    }
+  }
+}
+*/
