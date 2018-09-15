6582cf608c7e472c403ab385e67d43d07b6be6d5
Yago Estevez <inbox@yagoestevez.com>
2018-09-15 20:44:02 +0200

    Add last sections

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ require( "babel-runtime/regenerator" );
 require( './index.html'              );
 require( './main.scss'               );
 
-const makeNavLinksSmooth = ( ) => {
+/** Smooth Scrolling **/
+(function navSmoothScrolling ( ) {
   const navLinks = document.querySelectorAll( '.nav-link' );
 
   for ( let n in navLinks ) {
@@ -19,9 +20,10 @@ const makeNavLinksSmooth = ( ) => {
       } );
     }
   }
-}
+})( );
 
-const spyScrolling = ( ) => {
+/** ScrollSpy **/
+(function scrollSpy( ) {
   const sections = document.querySelectorAll( '.section' );
   const navBar   = document.querySelector( '#navbar' );
 
@@ -32,7 +34,6 @@ const spyScrolling = ( ) => {
       navBar.classList.remove( 'bg-black' );
       navBar.classList.add( 'bg-transparent' );
     } else {
-      console.log( sections[ 0 ].offsetTop, scrollPos )
       navBar.classList.remove( 'bg-transparent' );
       navBar.classList.add( 'bg-black' );
     }
@@ -44,32 +45,28 @@ const spyScrolling = ( ) => {
         const id = sections[ s ].id;
         document.querySelector( '.active' ).classList.remove( 'active' );
         document.querySelector( `a[href*=${ id }]` ).parentNode.classList.add( 'active' );
+        document.querySelector( `#${ id }` ).classList.add( 'show' );
       }
   }  
+})( );
+
+/** Carousel **/
+const numOfSlides = Object.keys( document.querySelectorAll( '.carousel-item' ) ).length;
+const changeSlide = ( forward = true ) => {
+  const currentSlide = document.querySelector( '.carousel-item.active' );
+  const currentID    = +currentSlide.id.slice( currentSlide.id.length-1 );
+  const nextSlide    = forward
+    ? currentID === numOfSlides
+      ? document.querySelector( `#carousel-item-1` )
+      : document.querySelector( `#carousel-item-${ currentID + 1 }` )
+    : currentID === 1
+      ? document.querySelector( `#carousel-item-${ numOfSlides }` )
+      : document.querySelector( `#carousel-item-${ currentID - 1 }` );
+  currentSlide.classList.remove( 'active' );
+  nextSlide.classList.add( 'active' );
 }
 
-makeNavLinksSmooth( );
-spyScrolling( );
-
-
-/*
-const sections = document.querySelectorAll( '.section' );
-
-window.onscroll = ( ) => {
-  const scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
-
-  for ( let s in sections ) {
-    if ( sections.hasOwnProperty( s ) ) {
-      if ( sections[ s ].offsetTop <= scrollPos ) {
-        const id   = sections[ s ].id;
-        const curr = document.querySelector( '.active' );
-        const next = document.querySelector( `a[href*=${ id }]` );
-        if ( curr.id !== id ) {
-          curr.classList.remove( 'active' );
-          next.parentNode.classList.add( 'active' );
-        }
-      }
-    }
-  }
-}
-*/
+document.querySelector( '.carousel-control-prev' )
+        .addEventListener( 'click', ( ) => changeSlide( false ) );
+document.querySelector( '.carousel-control-next' )
+        .addEventListener( 'click', ( ) => changeSlide( ) );
